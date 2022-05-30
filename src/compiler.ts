02f6bdd641a77e496a4b439b71ed32343184b205
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-05-30 19:37:39 +0900

    implement compiler

---
+++ src/compiler.ts
@@ -0,0 +1,100 @@
+import TS, { factory, SyntaxKind, TypeElement, TypeNode } from "typescript"
+import { Root, Section, Token, Variable } from "./tokenizer"
+
+interface CompilerOptions {
+  noLambdaTypeToVariable: boolean
+  noLambdaTypeToSection: boolean
+  noImplicitCaptureGlobalVariable: boolean
+}
+
+export function compile(
+  token: Root,
+  option: Readonly<CompilerOptions>
+): TS.TypeLiteralNode {
+  return merge(token.children, option)
+}
+
+function transform(
+  token: Token,
+  option: Readonly<CompilerOptions>
+): TS.TypeElement | null {
+  switch (token.type) {
+    case "variable":
+      return transformVariable(token, option)
+    case "nonFalseValue":
+      return PropertyType(`"${token.value}"`, BoolType())
+    case "section":
+      return transformSection(token, option)
+    case "text":
+    case "comment":
+    case "delimiter":
+    case "partial":
+    case "invertedSection":
+      return null
+    default:
+      throw new Error("Logic Exception")
+  }
+}
+
+function transformVariable(
+  token: Variable,
+  option: Readonly<Pick<CompilerOptions, "noLambdaTypeToVariable">>
+) {
+  if (option.noLambdaTypeToVariable) {
+    return PropertyType(token.value, StringType())
+  } else {
+    return PropertyType(token.value, UnionType(StringType(), LambdaType()))
+  }
+}
+
+function transformSection(
+  token: Section,
+  option: Readonly<CompilerOptions>
+): any {
+  if (token.children.filter((t) => t.type !== "text").length === 0) {
+    if (option.noLambdaTypeToSection) {
+      return PropertyType(token.value, BoolType())
+    } else {
+      return PropertyType(token.value, UnionType(BoolType(), LambdaType()))
+    }
+  } else {
+    if (option.noLambdaTypeToSection) {
+      return PropertyType(
+        token.value,
+        UnionType(BoolType(), ArrayType(merge(token.children, option)))
+      )
+    } else {
+      PropertyType(
+        token.value,
+        UnionType(
+          BoolType(),
+          LambdaType(),
+          ArrayType(merge(token.children, option))
+        )
+      )
+    }
+  }
+}
+
+function merge(
+  tokens: Token[],
+  option: Readonly<CompilerOptions>
+): TS.TypeLiteralNode {
+  const uniqueTokens = new Map(tokens.map((t) => [t.type + t.value, t]))
+  return factory.createTypeLiteralNode(
+    [...uniqueTokens.values()]
+      .map((t) => transform(t, option))
+      .filter((t) => t != null) as TypeElement[]
+  )
+}
+
+const StringType = () => factory.createKeywordTypeNode(SyntaxKind.StringKeyword)
+const BoolType = () => factory.createKeywordTypeNode(SyntaxKind.BooleanKeyword)
+const PropertyType = (name: string, type: TS.TypeNode) =>
+  factory.createPropertySignature(undefined, name, undefined, type)
+const LambdaType = () =>
+  factory.createFunctionTypeNode(undefined, [], StringType())
+const StringLiteralType = (literal: string) =>
+  factory.createStringLiteral(literal)
+const UnionType = (...types: TypeNode[]) => factory.createUnionTypeNode(types)
+const ArrayType = (type: TypeNode) => factory.createArrayTypeNode(type)
