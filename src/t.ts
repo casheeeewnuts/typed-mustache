67c5cffb8c86274899822921af1f494b83acd471
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-05-30 16:48:31 +0900

    wip

--- src/t.ts
+++ src/t.ts
@@ -1,13 +1,16 @@
-import mustache, { render } from "mustache"
+import TS from "typescript"
 import { tokenize } from "./tokenizer"
+import { transpile } from "./transpiler"
+import { emit } from "./emitter"
 
-const template = "hello! {{#age}}ki{{/age}}"
+const template =
+  "{{#users}}hello! {{name}} {{/users}}{{^users}}NO Users{{/users}} {{{license}}} {{#isPremium}}you!{{/isPremium}} {{#isAdmin?}}hi{{/isAdmin?}} {{! this is comment}} {{> element}} {{name}} {{=<m> </m>=}}"
+const test = "{{name}}"
 
-console.log(JSON.stringify(mustache.parse(template), null, 2))
-console.log(JSON.stringify(tokenize(template), null, 2))
-// console.log(
-//   mustache.render(template, {
-//     age: () => (text: string, render: (text: string) => string) =>
-//       `ki${render(text)}ki`,
-//   })
-// )
+// console.log(mustache.parse(template))
+// console.log(transpile(tokenize(test)))
+const printer = TS.createPrinter({
+  newLine: TS.NewLineKind.LineFeed,
+})
+
+console.log(emit(printer, transpile(tokenize(test)), "TemplateValues"))
