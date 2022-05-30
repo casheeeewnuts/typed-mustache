fef311596fd636a0a5c28885a7338719a9f2e8a0
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-05-30 16:40:01 +0900

    implement emitter.ts

---
+++ src/emitter.ts
@@ -0,0 +1,19 @@
+import TS, { factory, NodeFlags, SyntaxKind, TypeNode } from "typescript"
+
+export function emit(printer: TS.Printer, statement: TypeNode, name: string) {
+  const typeAlias = factory.createTypeAliasDeclaration(
+    undefined,
+    undefined,
+    name,
+    undefined,
+    statement
+  )
+
+  const source = TS.factory.createSourceFile(
+    [typeAlias],
+    TS.factory.createToken(SyntaxKind.EndOfFileToken),
+    NodeFlags.TypeExcludesFlags
+  )
+
+  return printer.printFile(source)
+}
