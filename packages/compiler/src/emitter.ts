9a277e74f4f0f3bcb6084e1185b6e971ab109053
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-05-31 17:23:21 +0900

    wip

--- packages/compiler/src/emitter.ts
+++ packages/compiler/src/emitter.ts
@@ -1,21 +1,30 @@
 import TS, { factory, NodeFlags, SyntaxKind, TypeNode } from "typescript"
 
-export function emit(printer: TS.Printer, statement: TypeNode, name: string) {
-  const typeAlias = factory.createTypeAliasDeclaration(
-    undefined,
-    [Export()],
-    name,
-    undefined,
-    statement
-  )
+export function emit(printerOptions: EmitterOptions) {
+  const printer = TS.createPrinter(printerOptions)
 
-  const source = TS.factory.createSourceFile(
-    [typeAlias],
-    TS.factory.createToken(SyntaxKind.EndOfFileToken),
-    NodeFlags.TypeExcludesFlags
-  )
+  return function (statement: TypeNode, name: string) {
+    const typeAlias = factory.createTypeAliasDeclaration(
+      undefined,
+      [Export()],
+      name,
+      undefined,
+      statement
+    )
 
-  return printer.printFile(source)
+    const source = TS.factory.createSourceFile(
+      [typeAlias],
+      TS.factory.createToken(SyntaxKind.EndOfFileToken),
+      NodeFlags.TypeExcludesFlags
+    )
+
+    return printer.printFile(source)
+  }
 }
+
+export type EmitterOptions = Omit<
+  TS.PrinterOptions,
+  "removeComments" | "newLine"
+>
 
 const Export = () => factory.createToken(SyntaxKind.ExportKeyword)
