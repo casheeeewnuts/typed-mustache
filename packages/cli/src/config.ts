7f17ed90b9dfa0191963901745820d73a9d48b00
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-06-08 11:31:04 +0900

    add config read functions

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -2,6 +2,8 @@ import {
   TranspilerOptions,
   EmitterOptions,
 } from "@casheeeewnuts/typed-mustache-compiler";
+import * as path from "path";
+import * as process from "process";
 
 type EmitOptions = {
   outDir?: string;
@@ -26,4 +28,55 @@ export function defineConfig(config: Partial<Options>): Options {
     ...defaultConfig,
     ...config,
   };
+}
+
+export const JSON_CONFIG_FILE_NAME = "typed-mustache.config.json";
+export const JS_CONFIG_FILE_NAME = "typed-mustache.config.js";
+
+export async function readConfig(): Promise<Options> {
+  const jsonConfigFilePath = path.join(process.cwd(), JSON_CONFIG_FILE_NAME);
+  const jsConfigFilePath = path.join(process.cwd(), JS_CONFIG_FILE_NAME);
+
+  return readJsonConfig(jsonConfigFilePath)
+    .catch(() => readJsConfig(jsConfigFilePath))
+    .catch(() => defaultConfig);
+}
+
+async function readJsonConfig(path: string): Promise<Options> {
+  try {
+    const config = require(path);
+
+    assertConfig(config);
+
+    return config;
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      throw e;
+    } else if (e instanceof Error) {
+      throw new IOError(path);
+    }
+    throw e;
+  }
+}
+
+async function readJsConfig(path: string): Promise<Options> {
+  try {
+    const config = import(path);
+
+    assertConfig(config);
+
+    return config;
+  } catch (e) {
+    throw e;
+  }
+}
+
+function assertConfig(config: unknown): asserts config is Options {
+  throw new Error("Invalid Configuration");
+}
+
+class IOError extends Error {
+  constructor(path: string) {
+    super(`failed to read/write file: ${path}`);
+  }
 }
