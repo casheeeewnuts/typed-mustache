48c713a067150ac47673cf165e9d710494f7e14c
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-06-28 22:17:26 +0900

    fix compile() to pass options

--- packages/compiler/src/compiler.ts
+++ packages/compiler/src/compiler.ts
@@ -1,23 +1,29 @@
-import { emit } from "./emitter"
-import { transpile } from "./transpiler"
+import { emit, EmitterOptions } from "./emitter"
+import { transpile, TranspilerOptions } from "./transpiler"
 import { tokenize } from "./tokenizer"
 
 export type MustacheTemplate = string
 export type TypeDeclaration = string
+export type CompilerOptions = EmitterOptions & TranspilerOptions
+
+const DEFAULT_COMPILER_OPTIONS: CompilerOptions = {
+  noLambdaTypeToVariable: true,
+  noWrappedFunction: false,
+  noImplicitCaptureGlobalVariable: true,
+  omitTrailingSemicolon: false,
+  noEmitHelpers: false,
+}
 
 export function compile(
   template: MustacheTemplate,
   name: string,
+  options?: CompilerOptions,
   tags?: [string, string]
 ): TypeDeclaration {
   const typeNode = transpile(tokenize(template, tags), {
-    noLambdaTypeToVariable: true,
-    noWrappedFunction: false,
-    noImplicitCaptureGlobalVariable: true,
+    ...DEFAULT_COMPILER_OPTIONS,
+    ...options,
   })
 
-  return emit({ omitTrailingSemicolon: false, noEmitHelpers: false })(
-    typeNode,
-    name
-  )
+  return emit({ ...DEFAULT_COMPILER_OPTIONS, ...options })(typeNode, name)
 }
