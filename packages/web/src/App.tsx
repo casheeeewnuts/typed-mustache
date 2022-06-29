00bd8e578d40f469a13d305ba7cccced35f35e8e
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-06-30 00:02:01 +0900

    wip

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,17 +1,32 @@
 import React, { useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import { compile } from "@casheeeewnuts/typed-mustache-compiler";
+import DotsH from "./dots-h.svg";
 
 function App() {
   const [template, setTemplate] = useState("Hello! {{name}}");
 
   return (
-    <div>
-      <header className="h-1 bg-red-600">
-        <div>typed-mustache</div>
+    <>
+      <header className="h-12 bg-[#3c3c3c]">
+        <div className="h-full grid grid-cols-4 gap-4">
+          <div className="col-span-3">
+            <div className="p-2 pl-8">
+              <p className="text-white font-black text-lg">
+                <b>typed-mustache</b>
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-4 gap-2">
+            <div className="col-span-3"></div>
+            <div className="border-2 items-center h-full">
+              <DotsH onClick={() => console.log("clicked")} />
+            </div>
+          </div>
+        </div>
       </header>
-      <div className="h-screen">
-        <div className="h-1/2">
+      <div className="h-screen dir-h">
+        <div>
           <Editor
             height="100%"
             defaultLanguage="mustache"
@@ -20,11 +35,11 @@ function App() {
             theme="vs-dark"
           />
         </div>
-        <div className="h-1/2">
+        <div>
           <Transpiled template={template} />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -37,7 +52,7 @@ const Transpiled: React.FC<{ template: string }> = ({ template }) => {
   return (
     <Editor
       height="100%"
-      theme="light"
+      theme="vs-dark"
       defaultLanguage="typescript"
       value={compileCache.current}
     />
--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "vs-dark": "#3c3c3c"
+        }
+    },
   },
   plugins: [],
 };
