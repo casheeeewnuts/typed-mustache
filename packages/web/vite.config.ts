66b25f040c83587075873261982fa85d5fd2b944
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2022-07-05 01:02:28 +0900

    remove favicon

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "typed-mustache",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
